6c5f6c16791d89de048d5718f8b79854b6fe50f5
ohohoi <ohohoi@sookmyung.ac.kr>
2020-08-10 20:35:53 +0900

    [4] 징검다리 건너기

--- 2019 카카오 개발자 겨울 인턴십/64602_징검다리 건너기.cpp
+++ 2019 카카오 개발자 겨울 인턴십/64602_징검다리 건너기.cpp
@@ -4,26 +4,31 @@
 
 using namespace std;
 
+bool check_mid_is_small(vector<int>stones, int mid, int k) {
+    int jump = 1;
+    bool small = false;
+    for (int i = 0; i < stones.size(); i++) {
+        if (stones[i] - mid <= 0) {
+            jump++;
+        }
+        else {
+            jump = 1;
+        }
+        if (jump > k) {
+            small = true;
+            break;
+        }
+    }
+    return small;
+}
+
 int solution(vector<int> stones, int k) {
     int left = 1;
     int right = *max_element(stones.begin(), stones.end());
 
     while (left <= right) {
         int mid = (left + right) / 2;
-        int jump = 1;
-        bool small = false;
-        for (int i = 0; i < stones.size(); i++) {
-            if (stones[i] - mid <= 0) {
-                jump++;
-            }
-            else {
-                jump = 1;
-            }
-            if (jump > k) {
-                small = true;
-                break;
-            }
-        }
+        bool small = check_mid_is_small(stones, mid, k);
         if (small) {
             right = mid - 1;
         }
