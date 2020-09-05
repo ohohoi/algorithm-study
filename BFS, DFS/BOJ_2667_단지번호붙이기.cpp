12f59b3db80350bd4ed067fc7b6b63a8f38b913b
ohohoi <ohohoi@sookmyung.ac.kr>
2020-09-05 14:01:01 +0900

    [5] 단지번호붙이기(map 대신 지역변수 사용)

--- BFS, DFS/BOJ_2667_단지번호붙이기.cpp
+++ BFS, DFS/BOJ_2667_단지번호붙이기.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <algorithm>
 
 using namespace std;
@@ -10,18 +9,18 @@ bool house_exists[25][25];
 bool visited[25][25];
 int x_direction[] = {0, 1, 0, -1};
 int y_direction[] = {-1, 0, 1, 0};
-map<int, int> complex_count_map;
-vector<int> complex_count_vector;
+int house_count = 0;
+vector<int> house_count_vec;
 
-void dfs(int x, int y, int complex_num) {
+void dfs(int x, int y) {
     visited[x][y] = true;
-    ++complex_count_map[complex_num];
+    ++house_count;
     for (int i = 0; i < 4; i++) {
         int next_x = x + x_direction[i];
         int next_y = y + y_direction[i];
         if (next_x >= 0 && next_x < n && next_y >= 0 && next_y < n) {
             if(house_exists[next_x][next_y] && !visited[next_x][next_y]) {
-                dfs(next_x, next_y, complex_num);
+                dfs(next_x, next_y);
             }
         }
     }
@@ -39,23 +38,20 @@ int main(int argc, const char * argv[]) {
         }
     }
     
-    int complex_num = 1;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if(house_exists[i][j] && !visited[i][j]) {
-                complex_count_map[complex_num] = 0;
-                dfs(i, j, complex_num++);
+                house_count = 0;
+                dfs(i, j);
+                house_count_vec.push_back(house_count);
             }
         }
     }
     
-    cout << complex_count_map.size() << '\n';
-    for (int i = 1; i <= complex_count_map.size(); i++) {
-        complex_count_vector.push_back(complex_count_map[i]);
-    }
-    sort(complex_count_vector.begin(), complex_count_vector.end());
-    for (int i = 0; i < complex_count_vector.size(); i++) {
-        cout << complex_count_vector[i] << '\n';
+    sort(house_count_vec.begin(), house_count_vec.end());
+    cout << house_count_vec.size() << '\n';
+    for (int i = 0; i < house_count_vec.size(); i++) {
+        cout << house_count_vec[i] << '\n';
     }
     
     return 0;
